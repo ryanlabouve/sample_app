0d853417150435c0adb578b0dea388e289a74ed2
Ryan LaBouve <imryanlabouve@gmail.com>
2013-03-09 15:08:38 -0600

    asdfasdlkfjasdflk

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,13 +4,52 @@ describe "Static pages" do
   describe "Home Page" do
     it "have the content 'Sample App'" do
       visit '/static_pages/home' 
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home' 
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
   describe "Help Page" do
     it "have the content 'Help'" do
       visit '/static_pages/help' 
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help' 
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+
   end
+
+   describe "About Page" do
+
+    it "have the content 'About Us'" do
+      visit '/static_pages/about' 
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about' 
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+    end
+
+  end 
+ describe "Contact Page" do
+
+    it "have the content 'Contact Us'" do
+      visit '/static_pages/contact' 
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact' 
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+
+  end 
+
 end
